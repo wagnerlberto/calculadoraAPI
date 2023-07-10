b198c224fe4f580d788d16c363a42e39b499d50c
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2023-07-10 19:36:00 -0300

    Verbo get, com o resultado da soma de dois valores, no 'ControladorPrincipal', implementado.

--- src/main/java/br/com/empresa/calculadoraAPI/controladores/ControladorPrincipal.java
+++ src/main/java/br/com/empresa/calculadoraAPI/controladores/ControladorPrincipal.java
@@ -1,10 +1,17 @@
 package br.com.empresa.calculadoraAPI.controladores;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculadora")
 public class ControladorPrincipal {
+  
+  @GetMapping
+  public String somar(double valor1, double valor2) {
+    double resultado = valor1 + valor2;
+    return "Resultado: " + resultado;
+  }
 
 }
