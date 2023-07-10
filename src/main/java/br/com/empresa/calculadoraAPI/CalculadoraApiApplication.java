426c26689a18b953d1638030af1eaef0c1c5b6a8
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2023-07-10 19:09:38 -0300

    first commit

---
+++ src/main/java/br/com/empresa/calculadoraAPI/CalculadoraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.empresa.calculadoraAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/empresa/calculadoraAPI/CalculadoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.calculadoraAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
